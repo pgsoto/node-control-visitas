ee1491dd6b5b5f49a7a02006c7bd50c40c5174dd
Pedro Soto Soto <dex.pgsoto@gmail.com>
2020-01-04 17:35:15 -0300

    conexion atlas mongo con variable

--- src/config/config.js
+++ src/config/config.js
@@ -30,10 +30,8 @@ let urlDB
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/control-visitas'
-        // urlDB = 'mongodb+srv://pgsoto:toor189@cluster0-cxwfd.mongodb.net/control-visitas?retryWrites=true&w=majority'
 } else {
-    // urlDB = process.env.MONGO_URI
-    urlDB = 'mongodb+srv://pgsoto:toor189@cluster0-cxwfd.mongodb.net/control-visitas?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB
 
